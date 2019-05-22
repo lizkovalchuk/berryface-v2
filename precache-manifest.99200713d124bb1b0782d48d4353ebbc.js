ed336a68d24512753614ce10050c62aa44bb3174
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2019-05-21 20:35:36 -0400

    Updates

---
+++ precache-manifest.99200713d124bb1b0782d48d4353ebbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "927b70b9a6ef9f386545d321d05540f8",
+    "url": "/berryface-v2/static/media/logo.927b70b9.png"
+  },
+  {
+    "revision": "0a72f797b879ff7672ab",
+    "url": "/berryface-v2/static/js/runtime~main.0a72f797.js"
+  },
+  {
+    "revision": "aeb2b20e9ebc241e4809",
+    "url": "/berryface-v2/static/js/main.aeb2b20e.chunk.js"
+  },
+  {
+    "revision": "5317e91be5f039e73f02",
+    "url": "/berryface-v2/static/js/2.5317e91b.chunk.js"
+  },
+  {
+    "revision": "aeb2b20e9ebc241e4809",
+    "url": "/berryface-v2/static/css/main.a35f1c8a.chunk.css"
+  },
+  {
+    "revision": "d790332ccc39a7fedf0fd4d2299ad865",
+    "url": "/berryface-v2/index.html"
+  }
+];
