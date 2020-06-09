31e04ad3a645f36665f2fde95e2831a584cbc6e9
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2020-06-09 15:40:23 -0400

    temperature gauge added

--- src/views/ProfilePage/ProfilePage.jsx
+++ src/views/ProfilePage/ProfilePage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import dayjs from 'dayjs';
 import {
   LineChart,
   Line,
@@ -10,6 +9,7 @@ import {
   Tooltip,
   Legend
 } from "recharts";
+import Thermometer from 'react-thermometer-component'
 
 // nodejs library that concatenates classes
 import classNames from "classnames";
@@ -18,6 +18,8 @@ import withStyles from "@material-ui/core/styles/withStyles";
 // @material-ui/icons
 import Sun from "@material-ui/icons/Brightness6";
 import Bubble from "@material-ui/icons/BubbleChart";
+import History from "@material-ui/icons/History"
+import Timeline from "@material-ui/icons/Timeline"
 // core components
 import Header from "components/Header/Header.jsx";
 import Footer from "components/Footer/Footer.jsx";
@@ -36,9 +38,6 @@ import "assets/scss/custom-style/profile-page.scss";
 // api
 import { entriesFilter } from "../../api/api";
 
-// helper functions
-import sum from '../../helpers/sums/sum';
-
 // custom components
 import CustomTooltip  from '../../components/CustomTooltip/tooltip'
 
@@ -46,7 +45,7 @@ class ProfilePage extends React.Component {
 
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
+    this.historyChange = this.historyChange.bind(this);
 
     this.state = {
         data: [],
@@ -55,7 +54,7 @@ class ProfilePage extends React.Component {
     }
   }
 
-  handleChange = async (event) => {
+  historyChange = async (event) => {
     const param = event.target.value;
     this.setState({value: param});
     let result = await entriesFilter(param);
@@ -143,8 +142,39 @@ class ProfilePage extends React.Component {
                     color="primary"
                     tabs={[
                       {
-                        tabButton: "Temperature",
+                        tabButton: "Current",
                         tabIcon: Sun,
+                        tabContent: (
+                          <GridContainer
+                            justify="center"
+                            id="profile__GridContainer_current"
+                          >
+                            <GridItem xs={12} sm={6}>
+                              <Thermometer
+                                class="testing"
+                                theme="light"
+                                value="18"
+                                max="40"
+                                steps="3"
+                                format="°C"
+                                size="large"
+                                height="300"
+                              />
+                            </GridItem>
+                            <GridItem xs={12} sm={6}>
+                              <LiquidGauge
+                              class="testing"
+                                value={this.state.humidity}
+                                percent="%"
+                                textSize="1"
+                              />
+                            </GridItem>
+                          </GridContainer>
+                        )
+                      },
+                      {
+                        tabButton: "Historical",
+                        tabIcon: History,
                         tabContent: (
                           <GridContainer
                             id="profile__GridContainer_temperature"
@@ -152,7 +182,7 @@ class ProfilePage extends React.Component {
                           >
                             <GridItem xs={12} sm={12} md={12}>
                               <div id="profile__GridContainer_filter">
-                                <select value={this.state.value} onChange={this.handleChange}>                              
+                                <select value={this.state.value} onChange={this.historyChange}>                              
                                   <option value="TEN_LATEST">Last 10 entries</option>
                                   <option value="25_LATEST">Last 25 entries</option>
                                   <option value="50_LATEST">Last 50 entries</option>
@@ -194,21 +224,52 @@ class ProfilePage extends React.Component {
                         )
                       },
                       {
-                        tabButton: "Humidity",
-                        tabIcon: Bubble,
+                        tabButton: "Averages",
+                        tabIcon: Timeline,
                         tabContent: (
                           <GridContainer
+                            id="profile__GridContainer_temperature"
                             justify="center"
-                            id="profile__GridContainer_humidity"
                           >
-                            <GridItem xs={12} sm={12} md={8}>
-                              <div id="profile__div_humidityContainer">
-                                <LiquidGauge
-                                  value={this.state.humidity}
-                                  percent="%"
-                                  textSize="1"
-                                />
+                            <GridItem xs={12} sm={12} md={12}>
+                              <div id="profile__GridContainer_filter">
+                                {/* <select value={this.state.value} onChange={this.averageChange}>                              
+                                  <option value="TEN_LATEST">Last 10 entries</option>
+                                  <option value="25_LATEST">Last 25 entries</option>
+                                  <option value="50_LATEST">Last 50 entries</option>
+                                </select> */}
                               </div>
+                            </GridItem>
+                            <GridItem xs={12} sm={12} md={12}>
+                              <LineChart
+                                width={600}
+                                height={300}
+                                data={this.state.data}
+                                margin={{
+                                  top: 5,
+                                  right: 30,
+                                  left: 20,
+                                  bottom: 5
+                                }}
+                              >
+                                <XAxis dataKey="formattedDate" />
+                                <YAxis />
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <Tooltip content={<CustomTooltip />} />
+                                <Legend />
+                                <Line
+                                  type="monotone"
+                                  dataKey="temperature"
+                                  stroke="#DC143C"
+                                  activeDot={{ r: 8 }}
+                                />
+                                <Line
+                                  type="monotone"
+                                  dataKey="humidity"
+                                  stroke="#8884d8"
+                                  activeDot={{ r: 8 }}
+                                />
+                              </LineChart>
                             </GridItem>
                           </GridContainer>
                         )
